085de86074f9fbc501ec9befed45016235f3ca18
Donghee Han <poohaha8974@gmail.com>
2025-03-25 16:49:00 +0900

    nav 코드 최적화 접근성 추가

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -4,59 +4,76 @@ import { Search } from "react-bootstrap-icons";
 import { Link, useHistory } from "react-router-dom";
 
 const Nav = ({ onSearch, list }) => {
-  const searchRef = useRef();
-  const history = useHistory();
+  const searchRef = useRef(); // 검색 입력 필드를 참조하기 위한 useRef 훅
+  const history = useHistory(); // 페이지 이동을 위한 useHistory 훅
 
+  // 검색 버튼 클릭 시 호출되는 함수
   const handleOnClick = () => {
-    onSearch(searchRef.current.value);
-    searchRef.current.value = "";
-    history.push("./search");
+    onSearch(searchRef.current.value); // 검색어를 onSearch 함수에 전달
+    searchRef.current.value = ""; // 검색 입력 필드 초기화
+    history.push("/search"); // 검색 결과 페이지로 이동
   };
 
-  const handleOnKeyPress = (event) => {
+  // 검색 입력 필드에서 Enter 키를 누를 때 호출되는 함수
+  const handleOnKeyDown = (event) => {
     if (event.key === "Enter") {
-      onSearch(searchRef.current.value);
-      searchRef.current.value = "";
-      history.push("./search");
+      onSearch(searchRef.current.value); // 검색어를 onSearch 함수에 전달
+      searchRef.current.value = ""; // 검색 입력 필드 초기화
+      history.push("/search"); // 검색 결과 페이지로 이동
     }
   };
 
   const count = list.length;
 
   return (
-    <nav className={styles.navWrap}>
+    <nav
+      className={styles.navWrap}
+      role="navigation"
+      aria-label="Main Navigation"
+    >
       <div className={styles.nav}>
         <img
           className={styles.logo}
           src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
-          alt="logo"
+          alt="TMDB Logo"
         />
         <div className={styles.searchBox}>
           <input
             className={styles.input}
-            ref={searchRef}
+            ref={searchRef} // 검색 입력 필드를 참조
             type="search"
             placeholder="영화, TV 시리즈, 인물 검색"
-            onKeyPress={handleOnKeyPress}
+            onKeyDown={handleOnKeyDown}
+            aria-label="검색어 입력"
           />
-          <button onClick={handleOnClick} className={styles.search}>
+          <button
+            onClick={handleOnClick}
+            className={styles.search}
+            aria-label="검색"
+          >
             <Search />
           </button>
         </div>
         <ul className={styles.menu}>
           <li className={styles.menuLink}>
-            <Link exact="true" to="/">
+            <Link exact="true" to="/" aria-label="홈">
               홈
             </Link>
           </li>
           <li className={styles.menuLink}>
-            <Link to="/movie">영화</Link>
+            <Link to="/movie" aria-label="영화">
+              영화
+            </Link>
           </li>
           <li className={styles.menuLink}>
-            <Link to="/tv">TV 시리즈</Link>
+            <Link to="/tv" aria-label="TV 시리즈">
+              TV 시리즈
+            </Link>
           </li>
           <li className={styles.menuLink}>
-            <Link to="/my_list">My List</Link>
+            <Link to="/my_list" aria-label="My List">
+              My List
+            </Link>
             <span className={styles.count}>{count}</span>
           </li>
         </ul>
